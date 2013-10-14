9c55e46b4038294c10f2e47cddb7381bc9db9c8c
hhd418 <hhd418@gmail.com>
2013-10-14 10:58:54 -0500

    Finsih through extensions Frontend Day1.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsApl::Application.config.secret_key_base = 'd0c059f7befc852599a6523f7788c594a04363b842b1d9c3b51cb40e62475905aa98e793b5f8b219e4be3feb53922e7886862a66378f04dfedee669459fa1576'
